07b57ef041cbc96e9f4e4931d72bfa709d550a43
bdew <blackdew@gmail.com>
2014-01-26 02:20:35 +0200

    Adding resources

--- src/net/bdew/deepcore/gui/Textures.scala
+++ src/net/bdew/deepcore/gui/Textures.scala
@@ -18,6 +18,16 @@ object Textures {
   val tankOverlay = new TextureLocation(texture, 10, 0)
   val powerFill = new TextureLocation(texture, 0, 0)
   val slotSelect = new TextureLocation(texture, 20, 0)
+
+  object Button16 {
+    val base = new TextureLocation(texture, 20, 18)
+    val hover = new TextureLocation(texture, 36, 18)
+    val rsOn =  new TextureLocation(texture, 37, 35)
+    val rsOff =  new TextureLocation(texture, 21, 35)
+    val enabled =  new TextureLocation(texture, 53, 35)
+    val disabled =  new TextureLocation(texture, 69, 35)
+  }
+
   def greenProgress(width: Int) = new TextureLocation(texture, 136 - width, 59)
   def whiteProgress(width: Int) = new TextureLocation(texture, 136 - width, 74)
 }
