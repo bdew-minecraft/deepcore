39fea0c2f3febc39b30a22fa69abe6b302f57005
bdew <blackdew@gmail.com>
2014-02-13 03:15:02 +0200

    MJ module should render different texture for unconnected sides

--- src/net/bdew/deepcore/blocks/mjOutput/BlockMjOutput.scala
+++ src/net/bdew/deepcore/blocks/mjOutput/BlockMjOutput.scala
@@ -10,5 +10,24 @@
 package net.bdew.deepcore.blocks.mjOutput
 
 import net.bdew.deepcore.multiblock.block.BlockOutput
+import net.minecraft.util.Icon
+import net.minecraft.world.IBlockAccess
+import net.minecraftforge.common.ForgeDirection
+import cpw.mods.fml.relauncher.{Side, SideOnly}
+import net.minecraft.client.renderer.texture.IconRegister
 
-class BlockMjOutput extends BlockOutput("MJOutput", "PowerOutput", classOf[TileMjOutput])
+class BlockMjOutput extends BlockOutput("MJOutput", "PowerOutput", classOf[TileMjOutput]) {
+  var enabledIcon: Icon = null
+
+  override def getBlockTexture(world: IBlockAccess, x: Int, y: Int, z: Int, side: Int) =
+    if (getTE(world, x, y, z).canConnectoToFace(ForgeDirection.values()(side)))
+      enabledIcon
+    else
+      blockIcon
+
+  @SideOnly(Side.CLIENT)
+  override def registerIcons(ir: IconRegister) {
+    blockIcon = ir.registerIcon("deepcore:mjoutput/disabled")
+    enabledIcon = ir.registerIcon("deepcore:mjoutput/main")
+  }
+}
